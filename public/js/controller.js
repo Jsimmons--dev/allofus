d1b5361b5e20a191ef0fbac058fcbc8a85c2c51a
Josh <jsimmons@udel.edu>
2015-08-15 14:45:50 +0000

    updated to use github

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,7 +1,18 @@
-var socialMediaApp = angular.module('socialMediaApp', []);
+var socialMediaApp = angular.module('socialMediaApp', ["firebase"]);
 
-socialMediaApp.controller('profileController',function($scope){
-  $scope.profile = {
-    'name':'Jess'
-  }
-});
+socialMediaApp.controller('profileController',function($scope,$firebaseAuth){        
+	var users = new Firebase("https://allofus.firebaseio.com/users");
+
+	var auth = $firebaseAuth(users);
+	auth.$authWithOAuthPopup("github").then(function(authData) {
+		console.log("Logged in as:",authData.uid);
+		$scope.authData = authData;
+		console.log(authData);
+	}).catch(function(error) {
+	console.log("Authentication Failed:",error);
+	});
+
+	//var corey = users.child('cshan-dev');
+	//var coreyFirst = corey.child('first');
+	//$firebaseObject(corey).$bindTo($scope,"corey");
+		});
